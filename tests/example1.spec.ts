dcb7805e73fb50a5cf659a22301a6015febc7922
KateJou <katejou@viridiasolutions.com>
2024-01-15 09:25:06 +0800

    init

---
+++ tests/example1.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('navigation', () => { // Learn to use describe
+
+  test.beforeEach(async ({ page }) => { // Learn to use before each
+    // Go to the starting url before each test.
+    await page.goto('https://playwright.dev/');
+  });
+
+  test('has title', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+  
+  test('get started link', async ({ page }) => {
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+  
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+});
+
+
+
+
