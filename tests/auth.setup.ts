60a2de07d483b3df1e77c5c12973eb11ea685728
KateJou <katejou@viridiasolutions.com>
2024-01-16 17:04:31 +0800

    save

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,16 +1,14 @@
 import { test as setup, expect } from '@playwright/test';
 
 const authFile = 'playwright/.auth/user.json';
+const yourGithubUserName = '<fill in yours>';
+const yourGithubPassword = '<fill in yours>';
 
 setup('authenticate', async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
-//   await page.goto('https://github.com/login');
-//   await page.getByLabel('Username or email address').fill('katejou02@gmail.com');
-//   await page.getByLabel('Password').fill('qetq27155');
-//   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.goto('https://github.com/login');
-  await page.getByLabel('Username or email address').fill('<yours>');
-  await page.getByLabel('Password').fill('<yours>>');
+  await page.getByLabel('Username or email address').fill(yourGithubUserName);
+  await page.getByLabel('Password').fill(yourGithubPassword);
   await page.getByRole('button', { name: 'Sign in', exact: true }).click();
   // Wait until the page receives the cookies.
   //
@@ -21,6 +19,8 @@ setup('authenticate', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Home', exact: true })).toBeVisible();
 
   // End of authentication steps.
-
   await page.context().storageState({ path: authFile }); //<-- remember to save it up
 });
+
+
+
