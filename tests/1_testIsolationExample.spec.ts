2c5ae25f7526d8c2485bd1e84db442276618cb83
KateJou <katejou@viridiasolutions.com>
2024-01-19 17:26:34 +0800

    save

--- tests/1_testIsolationExample.spec.ts
+++ tests/1_testIsolationExample.spec.ts
@@ -1,17 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+//test.describe.configure({ mode: 'serial' }); // see 10_serialMode(retry+dependent).txt
+test.describe.configure({ retries: 1 });
 
+test('has title', async ({ page }) => { // if I don't do the serial mode, this will not be retry.
+  await page.goto('https://playwright.dev/');
+  
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started link', async ({ page }, testInfo) => {
   await page.goto('https://playwright.dev/');
 
+  let expectName = 'X'; 
+
+  if (testInfo.retry) { expectName = 'Get started' }; 
+
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: expectName }).click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
--- tests/5_authenticationExample.spec.ts
+++ tests/5_authenticationExample.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 // remove .skip when play auth
 // remove .fail when play auth
 test.skip('authentication', async ({ page }) => {
-  test.slow();
+  test.slow(); // Easy way to "triple" the default timeout
   test.fail(true,'this test is failing because I did not open the auth settings.');
   await page.goto('https://github.com/');
   await expect(page.getByRole('heading', { name: 'Latest changes' })).toBeVisible();
